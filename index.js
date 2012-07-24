900bd537f788ec73a368776ddaede52f9241a3bd
Siddharth Mahendraker <siddharth_mahen@me.com>
2012-07-24 18:51:49 +0300

    Added circular refs and weak refs to callbacks
    * Added weak reference support to callbacks.
    
    Once the client drops all references to a callback, and the callback is
    GC'd, the server will be notified of this and subsequently remove that
    callback's id from the remote callback pool. This prevents the remote
    callback pool from growing forever.
    
    * Added circular reference support.
    
    All objects sent as arguments to callbacks can now contain circular
    references.
    
    * Updated and improved documentation.

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var mrpc = require("./lib/mrpc.js");
 
+// Exports
 exports = module.exports = mrpc;
 
-// TODO: Pretty obvious, but document me anyways
+/*
+ * Creates a connection without a constructor.
+ *
+ * @param { Number } port
+ * @param { String } host (optional)
+ * @param { Function } cb (optional)
+ *
+ * @returns { mrpc } server
+ *
+ * @api public
+ */
+
 exports.connect = function(port, host, cb){
-    var m = mrpc().listen(0).on("ready", function(){
-        m.connect(port, host, cb);
-    });
+    return mrpc().listen(0).connect(port, host, cb);
+}
+
+/*
+ * Creates a mrpc server without a connection or a constructor.
+ *
+ * @param { Number } port
+ * @param { String } host (optional)
+ * @param { Function } cb (optional)
+ *
+ * @returns { mrpc } server
+ *
+ * @api public
+ */
+
+exports.listen = function(port, host, cb){
+    return mrpc().listen(port, host, cb);
 }
